640ea9a52f7ca3e50f93f2df567a92ee25b2f24b
Cerezfra <francescocerez@gmail.com>
2022-07-07 12:33:36 +0200

    Commit lel

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_neumorphic/flutter_neumorphic.dart';git
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const NeumorphicApp(
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
+      ),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      ),
       home: MyHomePage(),
     );
   }
@@ -29,9 +40,63 @@ class _MyHomePageState extends State<MyHomePage> {
     
     return Scaffold(
       // appBar:  AppBar(title: const Text("Neumorphic Weather")),
-      body: const Center(
-        child: Text("Neumorphic Weather Info"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Neumorphic Weather Info"),
+            NeumorphicButton(
+              onPressed: () {
+                debugPrint("onClick");
+              },
+              style: const  NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              padding: const EdgeInsets.all(12.0),
+              child: const Icon(
+                Icons.favorite_border,
+                color: _iconsColor(context),
+              ),
+            ),
+            NeumorphicButton(
+                margin: EdgeInsets.only(top: 12),
+                onPressed: () {
+                  NeumorphicTheme.of(context)!.themeMode =
+                      NeumorphicTheme.isUsingDark(context)
+                          ? ThemeMode.light
+                          : ThemeMode.dark;
+                },
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+                ),
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "Toggle Theme",
+                  style: TextStyle(color: _textColor(context)),
+                ),),
+            
+          ],
+        ),
       ),
     );
+  }
+  Color? _iconsColor(BuildContext context) {
+    final theme = NeumorphicTheme.of(context);
+    if (theme!.isUsingDark) {
+      return theme.current!.accentColor;
+    } else {
+      return null;
+    }
+  }
+
+  Color _textColor(BuildContext context) {
+    if (NeumorphicTheme.isUsingDark(context)) {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
   }
 }
